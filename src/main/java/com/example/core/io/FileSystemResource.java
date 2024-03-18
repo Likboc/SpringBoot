df933014b180f7157a578d88f59e16fba1499fd6
downloader <git@github.com:Likboc/min-downloader.git>
2024-03-18 13:10:04 +0800

    FileSystemResource.java

--- src/main/java/com/example/core/io/FileSystemResource.java
+++ src/main/java/com/example/core/io/FileSystemResource.java
@@ -1,14 +1,29 @@
 package com.example.core.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
- * file loader
+ * 系统文件加载器
  */
-public class FileSystemResource implements Resource{
+public class FileSystemResource implements Resource {
+
+    private final String path;
+    public FileSystemResource(String path) {
+        this.path = path;
+    }
+
+    /**
+     * 使用nio包完成
+     * @return
+     * @throws IOException
+     */
     @Override
     public InputStream getInputStream() throws IOException {
-        return null;
+        Path path = new File(this.path).toPath();
+        return Files.newInputStream(path);
     }
 }
